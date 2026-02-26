23343fa94a52144b5e9949f1f0d8acfbd8913258
dreamQAQC <dream.qaqc@gmail.com>
2026-02-26 17:21:23 +0900

    2026-02-26 - 최종 커밋

---
+++ Test Suites/Result.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>결과</description>
+   <name>Result</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>ed919de0-29d4-4e2e-839b-668d1b82cfb8</testSuiteGuid>
+   <testCaseLink>
+      <guid>8414e462-719e-43d0-905d-2a685270f451</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Registration</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
